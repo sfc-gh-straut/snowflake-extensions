9a2fa5e442fbbaf6ddef239258e45efa94a440b6
Steve Traut <steve.traut@snowflake.com>
2024-03-26 14:41:03 -0400

    Minor changes to tied code together for execution.

--- git/tutorial/sql/create-db.sql
+++ git/tutorial/sql/create-db.sql
@@ -8,9 +8,9 @@ CREATE OR REPLACE WAREHOUSE tutorial_git_wh
   
 USE DATABASE tutorial_git_db;
 CREATE OR REPLACE SCHEMA tutorial_git_schema;
-USE SCHEMA tutorial_git_schema;
+USE SCHEMA tutorial_git_db.tutorial_git_schema;
 
 CREATE OR REPLACE TABLE employees(id NUMBER, name VARCHAR, role VARCHAR);
-INSERT INTO employees (id, name, role) VALUES (1, 'Alice', 'op'), (2, 'Bob', 'dev'), (3, 'Cindy', 'dev');
+INSERT INTO employees (id, name, role) VALUES (1, 'Alice', 'op'), (2, 'Peter', 'dev'), (3, 'Cindy', 'dev');
 
 SELECT * FROM employees;
---
+++ git/tutorial/sql/create-procedure.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE PROCEDURE tutorial_filter_by_role(tableName VARCHAR, role VARCHAR)
+  RETURNS TABLE(id NUMBER, name VARCHAR, role VARCHAR)
+  LANGUAGE PYTHON
+  RUNTIME_VERSION = '3.8'
+  PACKAGES = ('snowflake-snowpark-python')
+  IMPORTS = ('@tutorial_git_db.tutorial_git_schema.snowflake_extensions/branches/main/git/tutorial/python/filter.py')
+  HANDLER = 'filter.filter_by_role';
